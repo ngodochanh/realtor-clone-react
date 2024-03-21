c322de0f9282221b686380a103183fffcf772431
Camel <qtrung792@gmail.com>
2024-03-21 10:14:27 +0700

    Add the listing information to the listing page and add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,12 +7,15 @@ import SwiperCore from 'swiper';
 import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/bundle';
+import { FaShare, FaBed, FaBath, FaSquareParking, FaChair } from 'react-icons/fa6';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 function Listing() {
   const params = useParams();
   const [listing, setListing] = useState([]);
   const [loading, setLoading] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -53,6 +56,79 @@ function Listing() {
             </SwiperSlide>
           ))}
       </Swiper>
+
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
+
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white gap-4 ">
+        <div className=" w-full h-[200px] lg-[400px]">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - $
+            {listing.offer
+              ? listing.discountedPrice && listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+              : listing.regularPrice && listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            {listing.type === 'rent' && ' / month'}
+          </p>
+
+          <p className="flex items-center mt-6 mb-3 font-semibold">
+            <FaMapMarkerAlt className="text-green-700 mr-1" />
+            {listing.address}
+          </p>
+
+          <div className="flex justify-start items-center space-x-4 w-full">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === 'rent' ? 'Rent' : 'Sale'}
+            </p>
+
+            {listing.offer && (
+              <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
+                ${listing.regularPrice - listing.discountedPrice} discount
+              </p>
+            )}
+          </div>
+          <p className="mt-3 mb-3">
+            <span>Description</span> - {listing.description}
+          </p>
+
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+            <li className="flex items-center whitespace-nowrap">
+              <FaBed className="text-lg mr-1" />
+              {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : '1 Bed'}
+            </li>
+
+            <li className="flex items-center whitespace-nowrap">
+              <FaBath className="text-lg mr-1" />
+              {listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : '1 Bath'}
+            </li>
+
+            <li className="flex items-center whitespace-nowrap">
+              <FaSquareParking className="text-lg mr-1" />
+              {listing.parking ? 'Parking spot' : 'No parking'}
+            </li>
+
+            <li className="flex items-center whitespace-nowrap">
+              <FaChair className="text-lg mr-1" />
+              {listing.furnished ? 'Furnished' : 'No furnished'}
+            </li>
+          </ul>
+        </div>
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+      </div>
     </main>
   );
 }
